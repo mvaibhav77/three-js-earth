5686186792cb427c866f593545041c7216f9a285
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2025-07-24 14:05:44 +0530

    feat: added the lights mesh for dark side of the earth

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef } from "react";
 import {
+  ACESFilmicToneMapping,
+  AdditiveBlending,
   DirectionalLight,
   Group,
   IcosahedronGeometry,
+  LinearSRGBColorSpace,
   Mesh,
+  MeshBasicMaterial,
   MeshStandardMaterial,
   PerspectiveCamera,
   Scene,
@@ -12,9 +16,14 @@ import {
 } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
 
-// 2K Textures taken from https://www.solarsystemscope.com/textures/
+// 2K Earth Textures taken from https://www.solarsystemscope.com/textures/
 import DayEarth from "./assets/textures/day_earth.jpg";
 import NightEarth from "./assets/textures/night_earth.jpg";
+
+// 1K Cloud and Bump Texture from https://www.solarsystemscope.com/textures/
+import Clouds from "./assets/textures/cloudMap.jpg";
+import Bump from "./assets/textures/bump_earth.jpg";
+import Spec from "./assets/textures/spec_earth.jpg";
 import getStarfield from "./3dComponents/getStartfield.js";
 
 const w = window.innerWidth;
@@ -44,6 +53,8 @@ function App() {
     rendererRef.current = renderer;
 
     currentRef.appendChild(renderer.domElement);
+    renderer.toneMapping = ACESFilmicToneMapping;
+    renderer.outputColorSpace = LinearSRGBColorSpace;
 
     // define camera and scene
     const fov = 75;
@@ -72,12 +83,29 @@ function App() {
     const earthMesh = new Mesh(earthGeo, mat);
     earthGroup.add(earthMesh);
 
+    const lightsMat = new MeshBasicMaterial({
+      map: loader.load(NightEarth),
+      blending: AdditiveBlending,
+    });
+    const lightsMesh = new Mesh(earthGeo, lightsMat);
+    earthGroup.add(lightsMesh);
+
+    // const cloudsMat = new MeshBasicMaterial({
+    //   map: loader.load(Clouds),
+    //   transparent: true,
+    //   opacity: 0.8,
+    //   blending: AdditiveBlending,
+    // });
+    // const cloudsMesh = new Mesh(earthGeo, cloudsMat);
+    // cloudsMesh.scale.setScalar(1.001);
+    // earthGroup.add(cloudsMesh);
+
     // Add Stars in the background
-    const stars = getStarfield();
+    const stars = getStarfield({ numStars: 3000 });
     scene.add(stars);
 
     // Add lighting (Sunlight Effect)
-    const sunLight = new DirectionalLight(0xffffff, 2.0);
+    const sunLight = new DirectionalLight(0xffffff, 2);
     sunLight.position.set(-2, 0.5, 1.5);
     scene.add(sunLight);
 
@@ -85,6 +113,7 @@ function App() {
       requestAnimationFrame(animate);
 
       earthMesh.rotation.y += 0.001;
+      lightsMesh.rotation.y += 0.001;
 
       renderer.render(scene, camera);
       controls.update();
